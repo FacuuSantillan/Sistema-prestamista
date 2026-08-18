7c21a496af82dfc72289a238ef48f5424e2d05eb
FacuCasa <facusantillan1@gmail.com>
2026-08-18 00:46:04 -0300

    axs

--- prestamos-app/vite.config.js
+++ prestamos-app/vite.config.js
@@ -4,9 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // 🔴 AGREGAR ESTA SECCIÓN
+  // Previene que Vite oculte errores de Rust en la consola
+  clearScreen: false,
   server: {
+    port: 1420,
+    strictPort: true,
     watch: {
+      // Le dice a Vite que ignore los cambios en la carpeta de Rust/Tauri
       ignored: ['**/src-tauri/**']
     }
   }
